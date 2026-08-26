5465f71f210c37b1ff52a148d2916564ff0c4cb7
Sumit Jagdale <sumitjagdale@Mac.lan>
2026-08-26 14:50:44 +0530

    Updates

--- assets/react-three-fiber.esm-9BH5Ntkr.js
+++ assets/react-three-fiber.esm-9BH5Ntkr.js
@@ -1,4 +1,4 @@
-import{r as je,g as i_,j as yi}from"./index-DAH4dUCP.js";/**
+import{r as je,g as i_,j as yi}from"./index-Bvaki-vX.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
